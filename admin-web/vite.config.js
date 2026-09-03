1b7817b462484a7072c3831247d990fd904a8d57
Murlens Mexelus <lens9798@gmail.com>
2026-09-02 23:56:59 -0400

    Add admin accounts page

--- admin-web/vite.config.js
+++ admin-web/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     server: {
+        host: "localhost",
         port: 5173,
+        hmr: {
+            host: "localhost",
+            port: 5173,
+            protocol: "ws",
+        },
     },
 });
